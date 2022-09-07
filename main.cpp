8b1a96ecdc47d0927d85b266fa947ef6e4d4a764
a01704231 <112184077+a01704231@users.noreply.github.com>
2022-09-07 00:32:05 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 // =================================================================
 //
 // File: main.cpp
-// Author:
-// Date:
+// Author: Gaddiel Lara Roldán A01704231
+// Date: 06/09/2022
 //
 // =================================================================
 #include <iostream>
@@ -15,6 +15,39 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main() {
+    int n;
+    cout << "n: ";
+    cin >> n;
+    int elemento;
+    vector <int> v1;
+
+    for (int x = 1; x <= n; x++){
+        cout << "elemento " << x << ": ";
+        cin >> elemento;
+        v1.push_back(elemento);
+    }
+
+    int q;
+    cout << "q: ";
+    cin >> q;
+    int buscar[q];
+    for (int y = 0; y < q; y++){
+        cout << "buscar elemento " << y+1 << ": ";
+        cin >> elemento;
+        buscar[y] = elemento;
+    }
+
+    vector <int> v2 = v1;
+    cout << bubbleSort(v2) << " ";
+    v2 = v1;
+    cout << insertionSort(v2) << " ";
+    v2 = v1;
+    cout << selectionSort(v2) << endl << endl;
+
+    for (int y = 0; y < q; y++){
+        cout << sequentialSearch(v2,buscar[y]) << " ";
+        cout << binarySearch(v2,buscar[y]) << endl;
+    }
 	return 0;
 }
